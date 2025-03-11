bb92757afa1ec0888435fd9c961d3f6241be18cf
Nguyen Duc Trung <ductrung.ftu@gmail.com>
2025-03-12 06:59:18 +0700

    update README.md

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -9,11 +9,11 @@ export const databaseProviders = [
         type: 'mysql',
         host: process.env.MYSQL_HOST,
         port: +process.env.MYSQL_PORT || 3306,
-        username: process.env.MYSQL_USERNAME,
+        username: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
+        synchronize: false,
       });
 
       return dataSource.initialize();
